e59caf19e025159f4ab395f0e96c8f1076e71dc4
Ricardo <ricardo.cabrera@cfe.mx>
2022-04-01 13:59:06 -0700

    translate to spanish the load file page

--- drop/drandrop.php
+++ drop/drandrop.php
@@ -7,9 +7,9 @@ include('scripts/php/banners/head.php');
 <link rel="stylesheet" href="style.css" />
 <div id="drop_file_zone" ondrop="upload_file(event)" ondragover="return false">
     <div id="drag_upload_file">
-        <p>Drop file here</p>
-        <p>or</p>
-        <p><input type="button" value="Select File" onclick="file_explorer();" /></p>
+        <p>Arrastre un archivo aqui</p>
+        <p>o</p>
+        <p><input type="button" value="Seleccione archivo" onclick="file_explorer();" /></p>
         <input type="file" id="selectfile" />
     </div>
 </div>
