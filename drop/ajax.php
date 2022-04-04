2357c65da22bc8008a354a6f0422702ab6a3fb47
Ricardo <ricardo.cabrera@cfe.mx>
2022-04-04 13:04:06 -0700

    setting date to uploaded files

--- drop/ajax.php
+++ drop/ajax.php
@@ -12,8 +12,9 @@ if($file_type != "txt"){
 if (!file_exists('uploads')) {
     mkdir('uploads', 0777);
 }
- 
-$filename = time().'_'.$_FILES['file']['name'];
+$date = date('Y-m-d');
+//$filename = time().'_'.$_FILES['file']['name'];
+$filename = $date.'_'.$_FILES['file']['name'];
  
 move_uploaded_file($_FILES['file']['tmp_name'], 'uploads/'.$filename);
  
