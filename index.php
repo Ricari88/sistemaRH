d2caa1f879a6957d60df028dc0e056a88cef66ab
Ricardo <ricardo.cabrera@cfe.mx>
2022-05-04 13:53:26 -0700

    working on form on landing page

--- index.php
+++ index.php
@@ -4,20 +4,33 @@
     include("scripts/php/checadas.php");
     include("scripts/php/queries/busquedas.php");
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Asistencias</title>
-</head>
-<body>
-    <header>
-        encabezado    
-    </header>
+<main>
+<div class="mt-2">
+  <div class="d-flex flex-row-reverse">
+    <div class="p-2"><a href="#" class="btn btn-primary">Iniciar Sesion</a></div>
+  </div>
+</div>
+<div class="container">
+  <div class="col-6">
+  <h2>Form Grid</h2>
+  <p>Create two form elements that appear side by side with .row and .col:</p>
+  <form action="/action_page.php">
+    <div class="row">
+      <div class="col">
+        <input type="text" class="form-control" id="email" placeholder="Enter email" name="email">
+      </div>
+      <div class="col">
+        <input type="password" class="form-control" placeholder="Enter password" name="pswd">
+      </div>
+    </div>
+    <button type="submit" class="btn btn-primary mt-3">Submit</button>
+  </form>
+  </div>
+  <div class="col-6">
+      result
+  </div>
+</div>
+</main>
 <?php
     include('scripts/php/banners/foot.php');
 ?>
-</body>
-</html>
--- scripts/php/banners/head.php
+++ scripts/php/banners/head.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Asistencias</title>
+    <link rel="stylesheet" href="styles/main/main.css">
     <link rel="stylesheet" href="styles/main/tabla.css">
     <link rel="stylesheet" href="styles/main/form.css">
     <!-- Bootstrap -->
@@ -12,3 +13,27 @@
     <script src="styles/bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
+
+    <header class="head-banner">
+        <div class="header">
+            <div class="">
+                <div class="row head-banner-control">
+                    <div class="col-4 logo">
+                        <img src="assets/img/logo-blanco.jpg" alt="Logo CFE" class="main-logo">
+                    </div>
+                    <div class="col-4 text-center">
+                        <div class="titulo">
+                            <h1 class="titulo-principal">
+                                Residencia General de Cerro Prieto
+                                <!-- <h3 class="titulo-secundario">
+                                    Departamento de Recursos Humanos
+                                </h3> -->
+                            </h1>
+
+                        </div>
+                    </div>
+                    <div class="col-4"></div>
+                </div>
+            </div>
+        </div>
+    </header>
